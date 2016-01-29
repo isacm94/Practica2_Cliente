b452f08c16426328f4bce1412326c8c2709c46fe
isacm94 <isacm94@gmail.com>
2016-01-29 19:55:32 +0100

    29/01/2016, seguir generando cartón

--- bingo.js
+++ bingo.js
@@ -7,5 +7,39 @@ function MostrarCarton(){
 	else{
 		$('#contenido').attr('style', '');//Mostramos el contenido
 		$('#pre-contenido').hide();
+
+		alert(getCarton());
 	}
 }
+
+function getCarton(){
+	var carton = new Array(3);
+
+	carton[0] = new Array(9);
+	carton[1] = new Array(9);
+	carton[2] = new Array(9);
+
+	for (i=0; i<3; i++)
+	{
+		for (j=0; j<9; j++)
+		{
+			switch(expression) {
+    case n:
+        code block
+        break;
+    case n:
+        code block
+        break;
+    default:
+        default code block
+}
+			carton[i][j] = getRandom(1, 99)
+		}
+	}
+
+	return carton;
+}
+
+function getRandom(min, max) {
+         return Math.round(Math.random()*(max-min)+parseInt(min));
+         }
